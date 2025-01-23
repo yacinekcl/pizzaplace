2e29bee6110ae9f4d369ac8b0f477a7d6f37eeaa
yacinekcl <yacine7kecili@gmail.com>
2025-01-23 12:55:06 +0100

    update

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function scrollToMenu() {
     document.getElementById('ourmenu').scrollIntoView({ behavior: 'smooth' });
   }
   
+  // Get the button
+var mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
