611fb2e7b948d17bd1a09901c7f6e25de1a35f62
yacinekcl <yacine7kecili@gmail.com>
2025-01-28 18:53:51 +0200

    home link

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ window.onscroll = function() {
 
 // When the user clicks on the button, scroll to the top of the document
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: 'smooth' }); 
 }
